0439de6dd24573d659e8c13304f6d2f98ca40878
NotArt <noterisarthur42@gmail.com>
2024-11-12 21:48:21 +0100

    ~ | Optimized strjoin

--- str/ft_strjoin.c
+++ str/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: anoteris <noterisarthur42@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 04:03:29 by anoteris          #+#    #+#             */
-/*   Updated: 2024/10/19 07:16:42 by anoteris         ###   ########.fr       */
+/*   Updated: 2024/11/12 21:44:00 by anoteris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*res;
+	int		len_s1;
+	int		len_s2;
 
-	res = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	res = malloc(len_s1 + len_s2 + 1);
 	if (!res)
 		return (NULL);
-	ft_strlcpy(res, s1, ft_strlen(s1) + 1);
-	ft_strlcat(res, s2, (ft_strlen(s1) + ft_strlen(s2) + 1));
+	ft_strlcpy(res, s1, len_s1 + 1);
+	ft_strlcat(res, s2, (len_s1 + len_s2 + 1));
 	return (res);
 }
